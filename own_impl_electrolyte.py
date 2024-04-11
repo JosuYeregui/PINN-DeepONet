c49beec43836a41b0e0c348b4693a3195255f813
JosuYeregui <josu.yeregui@outlook.com>
2024-04-11 22:36:52 +0200

    Electrolyte equations implemented

--- own_impl_electrolyte.py
+++ own_impl_electrolyte.py
@@ -1,4 +1,4 @@
-from scr.SPM import Solid_Phase
+from scr.SPM import Solid_Phase, Electrolyte
 from scr.pinn import FFNN
 from scr.sampling import Sampler
 from scr.utils import load_params
@@ -24,37 +24,39 @@ if __name__ == "__main__":
     # training_points = {"PDE": 1000, "IV": 50, "BC_Center": 50, "BC_Surf": 50}
     training_points = {"PDE": {"type": "PDE", "N": 1000},
                        "IV": {"type": "IV", "N": 50},
-                       "BC_Center": {"type": "BC", "N": 50, "BC_pos": 0.},
-                       "BC_Surf": {"type": "BC", "N": 50, "BC_pos": 1.}}
+                       "BC_Left": {"type": "BC", "N": 50, "BC_pos": 0.},
+                       "BC_Right": {"type": "BC", "N": 50, "BC_pos": 1.}}
     # validation_points = {"PDE": 20, "IV": 10, "BC_Center": 10, "BC_Surf": 10}
     validation_points = {"PDE": {"type": "PDE", "N": 20},
                          "IV": {"type": "IV", "N": 10},
-                         "BC_Center": {"type": "BC", "N": 10, "BC_pos": 0.},
-                         "BC_Surf": {"type": "BC", "N": 10, "BC_pos": 1.}}
+                         "BC_Left": {"type": "BC", "N": 10, "BC_pos": 0.},
+                         "BC_Right": {"type": "BC", "N": 10, "BC_pos": 1.}}
 
-    pos_weights = {"PDE": 1e4, "IV": 10., "BC_Center": 1., "BC_Surf": 10.}
-    neg_weights = {"PDE": 1e4, "IV": 10., "BC_Center": 1., "BC_Surf": 10.}
+    weights = {"PDE": 1., "IV": 10., "BC_Left": 10., "BC_Right": 10.}
 
     C_rates_tr = [0.3, 0.5, 0.6, 0.7, 1.]
     C_rates_val = [0.4, 0.8]
 
+    C_rates_tr = [1.]
+    C_rates_val = [1.]
+
     model = FFNN(3, 1)
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-    PINN_neg = Solid_Phase(model, parameters, criterion=RMSELoss, electrode="neg", weights=neg_weights)
+    PINN_elec = Electrolyte(model, parameters, criterion=RMSELoss, weights=weights)
 
     Sampler_tr = Sampler(training_points)
     Sampler_val = Sampler(validation_points)
 
     history = {"loss_tr": [], "losses_tr": [], "loss_val": [], "losses_val": [], "iteration": []}
 
-    print("Iter \t\t PDE \t IV \t BC Centre \t BC Surf \t\t\t PDE \t IV \t BC Centre \t BC Surf")
-    for j in range(20000 + 1):
+    print("Iter \t\t PDE \t IV \t BC Left \t BC Right \t\t\t PDE \t IV \t BC Left \t BC Right")
+    for j in range(5000 + 1):
 
-        PINN_neg.update_crate(np.random.choice(C_rates_tr))
-        loss_tr, losses_tr = PINN_neg.train_step(optimizer, Sampler_tr)
+        PINN_elec.update_crate(np.random.choice(C_rates_tr))
+        loss_tr, losses_tr = PINN_elec.train_step(optimizer, Sampler_tr)
 
-        PINN_neg.update_crate(np.random.choice(C_rates_val))
-        loss_val, losses_val = PINN_neg.evaluate(Sampler_val)
+        PINN_elec.update_crate(np.random.choice(C_rates_val))
+        loss_val, losses_val = PINN_elec.evaluate(Sampler_val)
 
         if j % 1000 == 0:
             print(j, "\t\t", losses_tr, "\t\t", losses_val)
@@ -64,29 +66,10 @@ if __name__ == "__main__":
             history["losses_val"].append(losses_val)
             history["iteration"].append(j)
 
-    # j_prev = j
-    # optimizer = torch.optim.LBFGS(model.parameters(), lr=0.01, max_iter=50)
+    # PINN_neg.save_model("models/negative_current.pt")
     #
-    # for j in range(j_prev, j_prev + 200 + 1):
-    #
-    #     PINN_pos.update_crate(np.random.choice(C_rates_tr))
-    #     loss_tr, losses_tr = PINN_pos.train_step(optimizer, Sampler_tr)
-    #
-    #     PINN_pos.update_crate(np.random.choice(C_rates_val))
-    #     loss_val, losses_val = PINN_pos.evaluate(Sampler_val)
-    #
-    #     if j % 10 == 0:
-    #         print(j, "\t\t", losses_tr, "\t\t", losses_val)
-    #         history["loss_tr"].append(loss_tr)
-    #         history["losses_tr"].append(losses_tr)
-    #         history["loss_val"].append(loss_val)
-    #         history["losses_val"].append(losses_val)
-    #         history["iteration"].append(j)
-
-    PINN_neg.save_model("models/negative_current.pt")
-
-    with open('models/negative_current.pkl', 'wb') as fp:
-        pickle.dump(history, fp)
+    # with open('models/negative_current.pkl', 'wb') as fp:
+    #     pickle.dump(history, fp)
 
     # Plot
     plt.figure()
@@ -101,54 +84,45 @@ if __name__ == "__main__":
     # Evaluation
     # param = pybamm.ParameterValues("ORegan2022")
     param = pybamm.ParameterValues("Chen2020")
-    PBM_model = pybamm.lithium_ion.SPM()
-    experiment = pybamm.Experiment(["Discharge at 0.9C for 10000 seconds or until 2.5 V"])
+    PBM_model = pybamm.lithium_ion.SPMe()
+    experiment = pybamm.Experiment(["Discharge at 1C for 10000 seconds or until 2.5 V"])
     sim = pybamm.Simulation(PBM_model, experiment=experiment, parameter_values=param)
     sol = sim.solve(initial_soc=1)
+
+    ce = sol["Electrolyte concentration [mol.m-3]"]
 
     # pos_SPM_r0 = sol['X-averaged positive particle concentration'].entries[0, :]
     # pos_SPM_r1 = sol['X-averaged positive particle concentration'].entries[-1, :]
 
-    c_s_n = sol["Negative particle concentration"]
-    c_s_p = sol["Positive particle concentration"]
-    r_n = sol["r_n [m]"].entries[:, 0, 0]
-    r_p = sol["r_p [m]"].entries[:, 0, 0]
     t = sol["Time [s]"].entries
     x = sol["x [m]"].entries[:, 0]
 
-    pos_SPM_r0 = c_s_p(r=r_p[0], t=t, x=x[-1])
-    pos_SPM_r1 = c_s_p(r=r_p[-1], t=t, x=x[-1])
+    ce_t0 = ce(t=t[0], x=x)
+    ce_tend = ce(t=t[-1], x=x)
 
     t_end = t[-1]
 
-    t = np.linspace(0, t_end, num=len(pos_SPM_r1))/3600.
+    t = np.linspace(0, t_end, num=len(ce_tend))/3600.
 
-    PINN_pos.update_crate(0.9)
+    PINN_elec.update_crate(1.)
 
-    bcs_sample_t = torch.linspace(0., 1., 1000)
-    bcs_sample_r = torch.ones_like(bcs_sample_t)
+    bcs_sample_x = torch.linspace(0., 1., 1000)
+    bcs_sample_t = torch.zeros_like(bcs_sample_x)
     bcs_sample_I = torch.ones_like(bcs_sample_t) * 0.9
-    bcs_sample = torch.stack([bcs_sample_t, bcs_sample_r, bcs_sample_I]).t()
-    c_bcs = PINN_pos(bcs_sample)
+    bcs_sample = torch.stack([bcs_sample_t, bcs_sample_x, bcs_sample_I]).t()
+    ce_PINN_t0 = PINN_elec(bcs_sample)
+
+    bcs_sample_t = torch.ones_like(bcs_sample_x)
+    bcs_sample = torch.stack([bcs_sample_t, bcs_sample_x, bcs_sample_I]).t()
+    ce_PINN_tend = PINN_elec(bcs_sample)
 
     plt.figure()
     plt.grid("on")
-    plt.plot(bcs_sample_t.detach().numpy()*PINN_pos.tc/3600., c_bcs.detach().numpy(), "k", label="PINN")
-    plt.plot(t, pos_SPM_r1, "r", label="Pybamm")
+    # plt.plot(bcs_sample_t.detach().numpy() * PINN_elec.tc / 3600., ce_PINN_t0.detach().numpy(), "k", label="PINN")
+    # plt.plot(t, ce_t0, "r", label="Pybamm")
+    plt.plot(bcs_sample_x.detach().numpy() * PINN_elec.tc / 3600., ce_PINN_tend.detach().numpy(), "k", label="PINN")
+    plt.plot(t, ce_tend/parameters["ce0"], "r", label="Pybamm")
     plt.legend()
-    plt.xlabel("t [h]")
-    plt.ylabel("x [-]")
-    plt.show()
-
-    bcs_sample_r = torch.zeros_like(bcs_sample_t)
-    bcs_sample_r0 = torch.stack([bcs_sample_t, bcs_sample_r, bcs_sample_I]).t()
-    c_bcs_r0 = PINN_pos(bcs_sample_r0)
-
-    plt.figure()
-    plt.grid("on")
-    plt.plot(bcs_sample_t.detach().numpy()*PINN_pos.tc/3600., c_bcs_r0.detach().numpy(), "k", label="PINN")
-    plt.plot(t, pos_SPM_r0, "r", label="Pybamm")
-    plt.legend()
-    plt.xlabel("t [h]")
+    plt.xlabel("r [-]")
     plt.ylabel("x [-]")
     plt.show()
--- scr/SPM.py
+++ scr/SPM.py
@@ -125,8 +125,7 @@ class Solid_Phase(PINN):
 
 class Electrolyte(PINN):
 
-    def __init__(self, model, parameters, criterion=nn.MSELoss(), c_rate=1.,
-                 electrode="pos", weights=None):
+    def __init__(self, model, parameters, criterion=nn.MSELoss(), c_rate=1., weights=None):
         super().__init__(model, criterion)
 
         self.params = parameters
@@ -134,7 +133,7 @@ class Electrolyte(PINN):
         self.C_rate = c_rate
         self.tc = (1./c_rate)*3600.
 
-        self.weights = {"PDE": 1., "IV": 1., "BC_Center": 1., "BC_Surf": 1.}
+        self.weights = {"PDE": 1., "IV": 1., "BC_Left": 1., "BC_Right": 1.}
         if weights is not None:
             self.weights = weights
 
@@ -146,10 +145,12 @@ class Electrolyte(PINN):
 
         loss = []
 
+        i_app = - self.C_rate * self.params["I_typ"] / self.params["A"]
+
         pde_sample = sampler.points["PDE"] * torch.Tensor([1., 1., self.C_rate])
         c_pde = self(pde_sample)
 
-        loss.append(self.weights["PDE"] * self.criterion(self._pde(pde_sample, c_pde),
+        loss.append(self.weights["PDE"] * self.criterion(self._pde(pde_sample, c_pde, i_app),
                                                          torch.zeros_like(c_pde)))
 
         iv_sample = sampler.points["IV"] * torch.Tensor([1., 1., self.C_rate])
@@ -157,17 +158,17 @@ class Electrolyte(PINN):
 
         loss.append(self.weights["IV"] * self.criterion(self._iv(c_iv), torch.zeros_like(c_iv)))
 
-        bcc_sample = sampler.points["BC_Center"] * torch.Tensor([1., 1., self.C_rate])
+        bcc_sample = sampler.points["BC_Left"] * torch.Tensor([1., 1., self.C_rate])
         c_bcc = self(bcc_sample)
 
-        loss.append(self.weights["BC_Center"] * self.criterion(self._bc_centre(bcc_sample, c_bcc),
+        loss.append(self.weights["BC_Left"] * self.criterion(self._bc(bcc_sample, c_bcc, i_app),
                                                                torch.zeros_like(c_bcc)))
 
-        bcs_sample = sampler.points["BC_Surf"] * torch.Tensor([1., 1., self.C_rate])
+        bcs_sample = sampler.points["BC_Right"] * torch.Tensor([1., 1., self.C_rate])
         c_bcs = self(bcs_sample)
 
-        loss.append(self.weights["BC_Surf"] *
-                    self.criterion(self._bc_surf(bcs_sample, c_bcs, -self.C_rate * self.params["I_typ"]),
+        loss.append(self.weights["BC_Right"] *
+                    self.criterion(self._bc(bcs_sample, c_bcs, i_app),
                                    torch.zeros_like(c_bcs)))
 
         hist = np.array([l_hist.detach().numpy() for l_hist in loss])
@@ -195,36 +196,55 @@ class Electrolyte(PINN):
 
         return dcdx
 
-    def _pde(self, x, c):
+    def _pde(self, x, c, i_app):
 
         dcdx = torch.autograd.grad(c, x, grad_outputs=torch.ones_like(c),
                                    create_graph=True)[0]
 
-        dr2Ndr = - torch.autograd.grad(dcdx[:, 1] * torch.pow(x[:, 1], 2), x, grad_outputs=torch.ones_like(dcdx[:, 1]),
-                                       create_graph=True)[0]
+        N = self._compute_flux(x, c, i_app)
 
-        return (dcdx[:, 0] * torch.pow(x[:, 1], 2) / self.tc + self.params["D_"+self.el_name] /
-                np.power(self.params["R_"+self.el_name], 2) * dr2Ndr[:, 1])
-
-    def _bc_centre(self, x, c):
-
-        dcdr = torch.autograd.grad(c, x, grad_outputs=torch.ones_like(c),
-                                   create_graph=True)[0]
-
-        return dcdr[:, 1]
-
-    def _bc_surf(self, x, c, i_app):
-
-        dcdr = - torch.autograd.grad(c, x, grad_outputs=torch.ones_like(c),
+        dNdx = torch.autograd.grad(N, x, grad_outputs=torch.ones_like(N),
                                      create_graph=True)[0]
 
-        def regularization(t):
-            return 0.5 * (1 + torch.tanh((t - self.tc*0.01/self.tc) / (self.tc*0.01/self.tc)))
+        L_n = self.params["L_n"] / self.params["L"]
+        L_p = self.params["L_p"] / self.params["L"]
+        L_s = self.params["L_s"] / self.params["L"]
 
-        return (- dcdr[:, 1] - regularization(x[:, 0]) * np.power(self.params["R_"+self.el_name], 2) *
-                i_app / (3 * self.params["eps_"+self.el_name] * self.params["D_"+self.el_name] *
-                         self.params["L_"+self.el_name] * self.params["F"] * self.params["A"] *
-                         self.params["c_"+self.el_name+"_max"]))
+        left = dcdx[:, 0] * self.params["ce0"] / self.tc
+        left[x[:, 1] < L_n] *= self.params["por_n"]
+        left[(L_n <= x[:, 1]) & (x[:, 1] <= L_n + L_s)] *= self.params["por_s"]
+        left[x[:, 1] > L_n + L_s] *= self.params["por_p"]
+
+        right = -dNdx[:, 1] / self.params["L"]
+        right[x[:, 1] < L_n] += i_app / (self.params["F"] * self.params["L_n"])
+        right[x[:, 1] > L_n + L_s] += - i_app / (self.params["F"] * self.params["L_p"])
+
+        return left - right
+
+    def _bc(self, x, c, i_app):
+
+        return self._compute_flux(x, c, i_app)
 
     def _iv(self, c0):
-        return c0 - self.params["ce0"]
+        return c0 - 1.
+
+    def _compute_flux(self, x, c, i_app):
+
+        dcdx = torch.autograd.grad(c, x, grad_outputs=torch.ones_like(c),
+                                   create_graph=True)[0]
+
+        L_n = self.params["L_n"] / self.params["L"]
+        L_p = self.params["L_p"] / self.params["L"]
+        L_s = self.params["L_s"] / self.params["L"]
+
+        term_1 = - dcdx[:, 1] * self.params["D_e"](c) * self.params["ce0"] / self.params["L"]
+        term_1[x[:, 1] < L_n] *= self.params["por_n"]
+        term_1[(L_n <= x[:, 1]) & (x[:, 1] <= L_n + L_s)] *= self.params["por_s"]
+        term_1[x[:, 1] > L_n + L_s] *= self.params["por_p"]
+
+        term_2 = (self.params["t_plus"] * i_app / self.params["F"]
+                  * torch.ones_like(term_1))  # * self.params["R"] * self.params["T"]
+        term_2[x[:, 1] < L_n] *= x[x[:, 1] < L_n, 1] / L_n
+        term_2[x[:, 1] > L_n + L_s] *= (1 - x[x[:, 1] > L_n + L_s, 1]) / L_p
+
+        return (term_1 + term_2)
