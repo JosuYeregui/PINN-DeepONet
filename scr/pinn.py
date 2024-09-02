09d6bc1a3299a842f13884d7db623ca6330cc8f6
JosuYeregui <72566623+JosuYeregui@users.noreply.github.com>
2024-09-02 17:14:37 +0200

    Commentaries added to PINN module

--- scr/pinn.py
+++ scr/pinn.py
@@ -3,47 +3,71 @@ from torch import nn
 
 
 class PINN(nn.Module):
+    """
+    Base wrapper for the PINN models used in the project, based on Pytorch. Contains the methods necessary to
+    initialize the NN architectures, execute training/evaluation steps, update the model and save/load the
+    trained weights.
+    """
 
     def __init__(self, model, criterion=nn.MSELoss()):
         super().__init__()
+        # Load NN and apply initialization
         self.model = model
-        self.model.apply(self.init_weights)
+        self.model.apply(self._init_weights)
 
         self.criterion = criterion
 
-    def init_weights(self, m):
-        if isinstance(m, nn.Linear):
-            nn.init.xavier_uniform_(m.weight)
-            m.bias.data.fill_(0.01)
-
     def forward(self, x):
-        # x = sampler.points[mode]
-        # if isinstance(self.model, DeepONet):
-        #     return self.model(x, sampler.N).flatter()
-        # else:
+        """
+        Performs the forward pass to a given input data.
+        :param x: Input tensor (or tuple in case of DeepONet), containing the spatial and condition information
+        :return: Model response to input data
+        """
         return self.model(x).flatten()
 
     def update_model(self, model):
+        """
+        Updates the NN architecture according to a precomputed model.
+        :param model: Input model substituting the contained one
+        """
         self.model = model
 
     def save_model(self, PATH):
+        """
+        Saves the pytorch model as .tp file with
+        :param PATH: Local path to save the model
+        """
         torch.save(self.model, PATH)
 
     def load_model(self, PATH):
-        self.model = torch.load(PATH)
-        self.model.eval()
+        """
+        Loads the pytorch model stored as .tp file if possible
+        :param PATH: Local path where model is stored
+        """
+        try:
+            self.model = torch.load(PATH)
+            self.model.eval()
+        except:
+            print("Could not load the model from " + PATH + "!")
 
     def compute_loss(self, points):
         raise NotImplementedError
 
     def train_step(self, optimizer, sampler):
+        """
+        Performs one training step to the NN and returns the step loss.
+        :param optimizer: PyTorch optimizer
+        :param sampler: Sampler object defined in scr/sampling.py
+        :return: Returns the overall loss and component loss after running the forward pass
+        """
 
+        # Set the model in training mode
         self.model.train()
+        # Zero gradients for every batch
+        optimizer.zero_grad()
 
         def get_loss():
-
-            # Zero your gradients for every batch!
-            optimizer.zero_grad()
+            # Forward pass to compute the loss
 
             # Compute the loss and its gradients
             loss, losses = self.compute_loss(sampler)
@@ -51,53 +75,57 @@ class PINN(nn.Module):
             return loss, losses
 
         def closure():
+            # Wrapper to avoid issues with the optimizer.step method used with the LBFGS optimizer
             loss, _ = get_loss()
             return loss
 
         # Adjust learning weights
         if isinstance(optimizer, torch.optim.LBFGS):
+            # LBFGS requires to perform the step inserting the loss function as argument
             optimizer.step(closure)
             self.model.eval()
             loss_tr, losses_tr = get_loss()
         else:
+            # Otherwise the step is performed normally
             loss_tr, losses_tr = get_loss()
             optimizer.step()
 
+        # Detach the loss values from the computational graph
         loss_tr = loss_tr.detach().numpy()
 
         return loss_tr, losses_tr
 
     def evaluate(self, sampler):
+        """
+        Performs an evaluation pass to return loss.
+        :param sampler: Sampler object defined in scr/sampling.py
+        :return: Returns the overall loss and component loss after running the forward pass
+        """
 
+        # Set the model in evaluation mode
         self.model.eval()
-        # Compute the loss and its gradients
+        # Compute the loss
         loss_val, losses_val = self.compute_loss(sampler)
         loss_val = loss_val.detach().numpy()
 
         return loss_val, losses_val
 
-
-class FFNN_old(nn.Module):
-    def __init__(self,input_size, output_size, hidden=32):
-        super(FFNN_old, self).__init__()
-        self.tanh = nn.Tanh()
-        self.l1 = nn.Linear(input_size, hidden)
-        self.l2 = nn.Linear(hidden, hidden)
-        self.l3 = nn.Linear(hidden, hidden)
-        # self.l4 = nn.Linear(hidden, hidden)
-        # self.l5 = nn.Linear(hidden, hidden)
-        # self.l6 = nn.Linear(hidden, hidden)
-        self.lout = nn.Linear(hidden, output_size)
-
-    def forward(self, x):
-        output = self.tanh(self.l1(x))
-        output = self.tanh(self.l2(output))
-        output = self.tanh(self.l3(output))
-        output = self.lout(output)
-        return output
+    @staticmethod
+    def _init_weights(self, m):
+        """
+        Initializes the weights of the NN (see Xavier Glorot initialization)
+        :param m: Element of the model, applies initialization if it is a nn.Linear type layer.
+        """
+        if isinstance(m, nn.Linear):
+            nn.init.xavier_uniform_(m.weight)
+            m.bias.data.fill_(0.01)
 
 
 class FFNN(nn.Module):
+    """
+    Implements a Feed-Forward Neural Network (FFNN) with a specified number of layers,
+    input dimension, output dimension, activation function, and dropout rate.
+    """
     def __init__(self, layers, input_dim, output_dim, activation=nn.Tanh, dropout=0.2):
         super(FFNN, self).__init__()
         self.layers = nn.ModuleList()
@@ -116,6 +144,11 @@ class FFNN(nn.Module):
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
+        """
+        Performs a forward pass through the FFNN.
+        :param x: Input data tensor of shape (batch_size, input_dim)
+        :return: Response tensor of shape (batch_size, output_dim)
+        """
         for layer in self.layers[:-1]:
             x = self.activation(self.dropout(layer(x)))
         x = self.layers[-1](x)
@@ -123,20 +156,32 @@ class FFNN(nn.Module):
 
 
 class DeepONet(nn.Module):
+    """
+    Implements a DeepONet architecture consisting of two sub-networks, one for encoding the input function
+    at a fixed number of sensors (branch net), and another for encoding the locations for the
+    output functions (trunk net), which should be able to learn operators accurately and efficiently from a
+    relatively small dataset
+    """
     def __init__(self, branch_layers, trunk_layers, dim_branch, dim_trunk, dim_int, dim_out,
                  activation=nn.Tanh, dropout=0.2):
         super(DeepONet, self).__init__()
-        self.layers = nn.ModuleList()
 
+        # The sub-networks are defined as standard FFNN
         self.branch = FFNN(branch_layers, dim_branch, dim_int, activation=activation, dropout=dropout)
         self.trunk = FFNN(trunk_layers, dim_trunk, dim_int, activation=activation, dropout=dropout)
         self.b = nn.Parameter(torch.zeros(1, dim_out))  # Initialize with zeros
 
     def forward(self, x):
-
+        """
+        Performs a forward pass through the DeepONet architecture.
+        :param x: Input data tensor of shape (batch_size, input_dim)
+        :return: Response tensor of shape (batch_size, output_dim)
+        """
+        # Forward passes the Branch net and Trunk net
         out_B = self.branch.activation(self.branch(x[1].T))
         out_T = self.trunk.activation(self.trunk(x[0]))
 
+        # Aggregate the results of both sub-networks and add the bias
         out_nn = out_B * out_T
         u_pred = torch.sum(out_nn, dim=-1) + self.b
         return u_pred
